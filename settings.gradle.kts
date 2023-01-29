bae92d1059ae58b5bcce3a3eac5c34c52d3d3426
Jake Wiseberg <jawiseberg@gmail.com>
2023-01-29 13:03:14 -0500

    created multiplatform kotlin project and linked environment setup details in README

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Spartan_Bank"
+include(":Spartan_Bank")
+include(":shared")
