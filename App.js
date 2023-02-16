42053212f4896f90a193f8a58badf0e5f97df6de
= <=>
2023-02-16 11:14:51 -0500

    Test view using NativeBase component library

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Box, Button } from "native-base";
 
 export default function App() {
+  let balance = 0.00
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+        <Box alignSelf="center" top="45%" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" backgroundColor= "gray.150">
+          <Text style={styles.titleText}>Case Cash Balance</Text>
+          <Text style={styles.baseText}>${balance}</Text>
+            <Button variant="subtle" colorScheme="green" onPress={() => console.log("Adding Case Cash")}>
+              <Text style={styles.baseText}>
+                Add Case Cash
+              </Text>
+            </Button>
+        </Box>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  baseText: {
+    fontFamily: 'notoserif',
+    fontSize: 20,
+    textAlign: 'left',
+  },
+  titleText: {
+    fontSize: 40,
+    fontWeight: 'bold',
   },
 });
