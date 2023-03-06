27d04dc5da01b83516810f2766b3c074f319038a
reecebw <reecebucknerwolfson@gmail.com>
2023-03-05 20:58:20 -0500

    Added register and forgot pw view, modifed look

--- App.js
+++ App.js
@@ -6,31 +6,32 @@ import { MD3LightTheme as DefaultTheme, Provider as PaperProvider } from 'react-
 import LoginView from './views/login';
 import ProfileView from './views/profile';
 import HomeView from './views/home';
+import RegisterView from './views/register';
+import ForgotPasswordView from './views/forgot_password';
+import { theme } from './core/theme'
 const Stack = createNativeStackNavigator();
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'tomato',
-    secondary: 'yellow',
-  },
-};
 
 export default function App() {
   return (
     <PaperProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Spartan Bank">
-          <Stack.Screen name="Spartan Bank" component={LoginView} />
+        <Stack.Navigator initialRouteName="Login" screenOptions={navigationOptions}>
+          <Stack.Screen name="Login" component={LoginView} />
           <Stack.Screen
             name="Home"
             component={HomeView}
             options={{ title: 'Welcome' }}
           />
           <Stack.Screen name="Profile" component={ProfileView} />
+          <Stack.Screen name="Register" component={RegisterView} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPasswordView} />
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
   );
+}
+
+const navigationOptions = {
+  headerStyle: { backgroundColor: theme.colors.primary },
+  headerTitleStyle: { color: 'white' },
 }
